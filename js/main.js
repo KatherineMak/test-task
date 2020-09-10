a79e5f4117ba4efcbe0175284a8291f7b29b80c6
KatherineMak <30404996+KatherineMak@users.noreply.github.com>
2020-09-10 14:41:04 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("dropdown").addEventListener('click', function () {
+    document.querySelector(".dropdown-menu").classList.toggle("is-active");
+});
+
+// dropdown.onclick(function () {
+//     document.querySelector(".dropdown-menu").toggleClass("is-active");
+// });
